f5ad5c7ef3a60fe2c867fc039ac52ef18db6d328
Grangclark <96944049+Grangclark@users.noreply.github.com>
2025-04-04 21:38:50 +0900

    String Literals

--- Strings and Characters/Strings and Characters.playground/Contents.swift
+++ Strings and Characters/Strings and Characters.playground/Contents.swift
@@ -1,3 +1,10 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+
+// 2025/04/04[金]
+// 文字列リテラル(String Literals)
+// 文字列リテラルはダブルクォーテーション(")で囲まれた一連の文字です。
+let someString = "Some string literal value"
+
