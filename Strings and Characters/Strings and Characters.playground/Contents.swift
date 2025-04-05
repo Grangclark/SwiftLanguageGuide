14b72ab452381f8b486bc85ae940a71adb9ff868
Grangclark <96944049+Grangclark@users.noreply.github.com>
2025-04-05 18:49:26 +0900

    Multiline String Literals

--- Strings and Characters/Strings and Characters.playground/Contents.swift
+++ Strings and Characters/Strings and Characters.playground/Contents.swift
@@ -8,3 +8,15 @@ var greeting = "Hello, playground"
 // 文字列リテラルはダブルクォーテーション(")で囲まれた一連の文字です。
 let someString = "Some string literal value"
 
+
+// 2025/04/05[土]
+// 複数行文字列リテラル(Multiline String Literals)
+// 複数行に渡った文字列が必要な場合、複数行文字列リテラルを使いましょう。
+// 一連の文字を 3 つのダブルクォーテーション(")で囲みます。
+let quotation = """
+The White Rabbit put on his spectacles.  "Where shall I begin,
+please your Majesty?" he asked.
+
+"Begin at the beginning," the King said gravely, "and go on
+till you come to the end; then stop."
+"""
