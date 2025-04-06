8a399f7637bdd8c82c353b11b32a92b7e3bad7eb
Grangclark <96944049+Grangclark@users.noreply.github.com>
2025-04-06 19:54:41 +0900

    Multiline String Literals

--- Strings and Characters/Strings and Characters.playground/Contents.swift
+++ Strings and Characters/Strings and Characters.playground/Contents.swift
@@ -20,3 +20,13 @@ please your Majesty?" he asked.
 "Begin at the beginning," the King said gravely, "and go on
 till you come to the end; then stop."
 """
+
+
+// 2025/04/06[日]
+// 複数行文字列リテラルは、全てを開始と終了のクォーテーションマークの間に含めます。
+// 文字列は、開始クォーテーションの次の行から始め、終了クォーテーションの 1 つ前の行で終了します。
+// つまり、文字列を改行で開始、終了することができません。
+let singleLineString = "These are the same."
+let multilineString = """
+These are the same.
+"""
