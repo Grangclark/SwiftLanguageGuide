788af1e2555d848474ebd69c57dd555430fa969b
Grangclark <96944049+Grangclark@users.noreply.github.com>
2025-06-18 09:11:23 +0900

    For-In Loops

--- Control Flow/Control Flow.playground/Contents.swift
+++ Control Flow/Control Flow.playground/Contents.swift
@@ -1,3 +1,17 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+// 2025/06/18[水]
+// For-Inループ(For-In Loops)
+// 配列のアイテムや数値の範囲、文字列の文字などのシーケンスに対してループ処理を行うために for-in ループを使います。
+// 下記の例は、for-in ループを使用して配列のアイテムにループ処理を行なっています。
+let names = ["Anna", "Alex", "Brian", "Jack"]
+for name in names {
+    print("こんにちは、　\(name)!")
+}
+// こんにちは、 Anna!
+// こんにちは、 Alex!
+// こんにちは、 Brian!
+// こんにちは、 Jack!
+
