93ba64f6aee0936cb7e3877babdfd76df7ba4452
Grangclark <96944049+Grangclark@users.noreply.github.com>
2025-06-19 11:55:36 +0900

    For-In Loops

--- Control Flow/Control Flow.playground/Contents.swift
+++ Control Flow/Control Flow.playground/Contents.swift
@@ -15,3 +15,19 @@ for name in names {
 // こんにちは、 Brian!
 // こんにちは、 Jack!
 
+
+
+// 2025/06/19[木]
+// 辞書のキーバリューペアにアクセスするためにもループ処理を使用することができます。
+// 辞書を繰り返す際に、辞書内の個々のアイテムは (key, value) のタプルとして返され、
+// for-in ループ内で使用するために、(key, value) のタプルの個々のメンバは明示的に命名した定数に展開できます。
+// 下記のコードは、辞書のキーは animalName 定数に、バリューは legCount 定数に展開されています。
+let numberOfLegs = ["spider": 8, "ant": 6, "cat": 4]
+for (animalName, legCount) in numberOfLegs {
+    print("\(animalName) には \(legCount) 本の足があります")
+}
+// cat には 4 本の足があります
+// ant には 6 本の足があります
+// spider には 8 本の足があります
+
+
