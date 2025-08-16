28f7722caa52ed0e4e1aa4de444ed84c1af72af9
Grangclark <96944049+Grangclark@users.noreply.github.com>
2025-08-16 17:32:15 +0900

    Matching Enumeration Values with a Switch Statement

--- Enumerations/Enumerations.playground/Contents.swift
+++ Enumerations/Enumerations.playground/Contents.swift
@@ -26,3 +26,28 @@ enum CompassPoint {
 // 列挙型で定義された値(north、south、east、west など)は、その列挙ケースです。
 // case キーワードを使用して、新しい列挙ケースを導入します。
 
+
+
+// 2025/08/16[土]
+// switch 文を使った列挙値のパターンマッチング(Matching Enumeration Values with a Switch Statement)
+// switch 文を使って、個々の列挙値をパターンマッチングできます。
+directionToHead = .south
+switch directionToHead {
+case .north:
+    print("多くの惑星には北があります")
+case .south:
+    print("ペンギンに気をつけて")
+case .east:
+    print("太陽が登る場所")
+case .west:
+    print("空が青い場所")
+}
+// ペンギンに気をつけて
+
+// このコードは次のように読むことができます。
+// 「directionToHead の値を検証してください。
+// .north に等しい場合は、「多くの惑星には北があります」と出力します。
+// .south に等しい場合は、「ペンギンに気をつけて」と出力してください」
+// …などなど。
+
+
