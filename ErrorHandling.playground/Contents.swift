00474e6b749c63cb9a322bbc97aafa72a3cc5959
Grangclark <96944049+Grangclark@users.noreply.github.com>
2025-12-22 09:00:59 +0900

    Error Handling

--- ErrorHandling.playground/Contents.swift
+++ ErrorHandling.playground/Contents.swift
@@ -16,3 +16,15 @@ enum VendingMachineError: Error {
     case insufficientFunds(coinsNeeded: Int)
     case outOfStock
 }
+
+
+
+
+// 2025年12月21日[日]
+// エラーをスローすると、予期しないことが発生し、通常の実行フローを続行できないことを示すことができます。
+// エラーをスローするには、throw 文を使用します。
+// 例えば、次のコードはエラーをスローして、自動販売機で 5 枚の追加のコインが必要だということを示します。
+throw VendingMachineError.insufficientFunds(coinsNeeded: 5)
+
+
+
