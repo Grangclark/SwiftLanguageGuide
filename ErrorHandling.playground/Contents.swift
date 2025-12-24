fc686c66cbaba498e6c8a7b61d1af1e8cfe41fba
Grangclark <96944049+Grangclark@users.noreply.github.com>
2025-12-24 09:27:21 +0900

    Propagating Errors Using Throwing Functions

--- ErrorHandling.playground/Contents.swift
+++ ErrorHandling.playground/Contents.swift
@@ -41,3 +41,45 @@ func cannotThrowErrors() -> String
 
 
 
+
+// 2025年12月24日[水]
+// 下記の例VendingMachine クラスに vend(itemNamed:) メソッドがあり、要求されたアイテムが利用できない場合、在庫がない場合、
+// または現在のお金が足りない場合に、適切な VendingMachineError をスローします:
+struct Item {
+    var price: Int
+    var count: Int
+}
+
+class VendingMachine {
+    var inventory = [
+        "Candy Bar": Item(price: 12, count: 7),
+        "Chips": Item(price: 10, count: 4),
+        "Pretzels": Item(price: 7, count: 11)
+    ]
+    var coinsDeposited = 0
+
+    func vend(itemNamed name: String) throws {
+        guard let item = inventory[name] else {
+            throw VendingMachineError.invalidSelection
+        }
+
+        guard item.count > 0 else {
+            throw VendingMachineError.outOfStock
+        }
+
+        guard item.price <= coinsDeposited else {
+            throw VendingMachineError.insufficientFunds(coinsNeeded: item.price - coinsDeposited)
+        }
+
+        coinsDeposited -= item.price
+
+        var newItem = item
+        newItem.count -= 1
+        inventory[name] = newItem
+
+        print("Dispensing \(name)")
+    }
+}
+
+
+
